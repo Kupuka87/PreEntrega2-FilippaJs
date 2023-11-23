22637864af85a4f46e1423c624ce55d6873b4293
marianofilippa <filippa.mariano@gmail.com>
2023-11-23 13:18:40 -0300

    se cargaron variables de compra en js

--- js/app.js
+++ js/app.js
@@ -1,91 +1,21 @@
-// Saludo al usuario
-hola();
+//Denominaciòn de variables
+const productos = [
+  {nombre: "mosqueton", precio: 5700};
+  {nombre: "placa", precio: 5700};
+  {nombre: "cordin", precio: 7900};
+  {nombre: "arnes", precio: 100000};
+  {nombre: "pedula", precio: 120000};
+  {nombre: "casco", precio: 115000};
+  {nombre: "cinta", precio: 2400};
+];
 
-// Comenzar el juego
-adivinameSiPodes();
+//Carrito
+let carrito = []
 
-// Inicio boxMsj
-function boxMensaje(mensaje) {
-  alert(mensaje);
+//Bienvenida 
+let seleccion = prompt ("Elige tus productos para iniciar en la escalda. Desea compar algo? Si o No")
+
+while (seleccion != "continuar"  && != "no")¨{
+  alert("por favor, elija la acción a seguir")
+  seleccion = prompt("Desea Comprar Algo, si o no")
 }
-
-// Fin boxMsj
-
-// Inicio Validar número de 1 a 100
-function controlNum(entrada) {
-  return Number.isInteger(entrada) && entrada >= 1 && entrada <= 100;
-}
-// Fin Validar número de 1 a 100
-
-// Función para saludar al usuario
-function hola() {
-  boxMensaje(
-    "Quiero jugar un juego. Tienes 6 intentos para adivinar el número secreto entre 1 y 100. Si fallas, habrá consecuencias. Comencemos..."
-  );
-}
-
-// Inicio Número Aleatorio
-function numeroRandom() {
-  return Math.floor(Math.random() * 100) + 1;
-}
-// Fin Número Aleatorio
-
-// Inicio Juego
-function adivinameSiPodes() {
-  const numeroAleatorio = numeroRandom();
-  let adivinado = false;
-
-  for (let intentos = 1; intentos <= 6; intentos++) {
-    let guess;
-
-    do {
-      let numeroIngresado = prompt(
-        `Intento ${intentos}: Adivina el número (entre 1 y 100). Te quedan ${
-          7 - intentos
-        } intentos:`
-      );
-      guess = parseInt(numeroIngresado);
-
-      if (numeroIngresado === null) {
-        boxMensaje("Has decidido cancelar el juego. Habrá consecuencias.");
-        boxMensaje(
-          "No sé cuáles, solo sé hacer estos cartelitos. Por ahora..."
-        );
-        boxMensaje("Que tengas un hermoso día");
-        return;
-      } else if (numeroIngresado.trim() === "") {
-        boxMensaje("Por favor, ingresa un número.");
-      } else if (!controlNum(guess)) {
-        boxMensaje("Por favor, ingresa un número entero válido entre 1 y 100.");
-      }
-    } while (!controlNum(guess));
-
-    if (guess === numeroAleatorio) {
-      adivinado = true;
-      boxMensaje(`¡Felicidades! Adivinaste el número en ${intentos} intentos.`);
-      break;
-    } else if (intentos < 5) {
-      if (guess < numeroAleatorio) {
-        boxMensaje("El número es mayor. Intenta de nuevo.");
-      } else if (guess > numeroAleatorio) {
-        boxMensaje("El número es menor. Intenta de nuevo.");
-      }
-    } else if (intentos === 5) {
-      if (guess < numeroAleatorio) {
-        boxMensaje("El número es mayor. Intenta de nuevo.");
-      } else if (guess > numeroAleatorio) {
-        boxMensaje("El número es menor. Intenta de nuevo.");
-      }
-      boxMensaje("¡Cuidado! El siguiente es tu último intento.");
-    } else if (intentos === 6) {
-      boxMensaje(
-        `Se acabaron los intentos. El número era ${numeroAleatorio}. Ahora las consecuencias`
-      );
-      boxMensaje("No sé cuáles, solo sé hacer estos cartelitos. Por ahora...");
-      boxMensaje("Que tengas un hermoso día");
-    }
-  }
-}
-// Fin Juego
-
-
